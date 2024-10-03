35a67d6af54332bc597cba6f0776a6fb18401dec
Jhonatan-Vegeta-Farfan <jhonatannfarfan@gmail.com>
2024-10-03 09:47:23 -0500

    123

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -1,21 +1,21 @@
 <?php
-class vistaModelo{
+class vistaModelo
+{
 
-    protected static function obtener_vista($vista){
-        $palabras_permitidas =['login','usuario','producto'];
-        if (in_array($vista,$palabras_permitidas)){
-            if (is_file("./views".$vista.".php")){
-                $contenido = "./views".$vista.".php";
-            }else{
+    protected static function obtener_vista($vista)
+    {
+        $palabras_permitidas = ['usuarios', 'nuevo-usuario','usuario','producto','nuevo-producto','producto'];
+        if (in_array($vista, $palabras_permitidas)) {
+            if (is_file("./views/" . $vista . ".php")) {
+                $contenido = "./views/" . $vista . ".php";
+            } else {
                 $contenido = "404";
+            }
+        } elseif ($vista == "login" || $vista == "index") {
+            $contenido = "login";
+        } else {
+            $contenido = "404";
         }
-    }elseif ($vista=="login" || $vista=="index"){
-        $contenido = "login";
-    }else{
-        $contenido = "404";
+        return $contenido;
     }
-    return $contenido;
 }
-}
-
-?>
