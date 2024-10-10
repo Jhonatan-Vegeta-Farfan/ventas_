fec40e4a737f1171f7b02907f4483bf793be68d1
Jhonatan-Vegeta-Farfan <jhonatannfarfan@gmail.com>
2024-10-10 12:04:30 -0500

    s

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -6,7 +6,7 @@ class vistaModelo
     {
         $palabras_permitidas = ['usuarios', 'nuevo-usuario','usuario','producto','nuevo-producto','producto','cantidad','carrito',
         'comentarios','contactanos','detalleproducto','libroreclamaciones','nosotros','novedades','pagofactura','perfil',
-        'rutadeenvio','suscribirte','ubicacion','descargarboleta','respuestadedescarga','respuestadeimprecion'];
+        'rutadeenvio','suscribirte','ubicacion','descargarboleta','respuestadedescarga','respuestadeimprecion','imprimirboleta'];
         if (in_array($vista, $palabras_permitidas)) {
             if (is_file("./views/" . $vista . ".php")) {
                 $contenido = "./views/" . $vista . ".php";
