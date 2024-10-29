ae0d4e6d76947785e7a16fd88347b55b8223f87e
Jhonatan-Vegeta-Farfan <jhonatannfarfan@gmail.com>
2024-10-29 10:12:05 -0500

    q

--- views/include/footer.php
+++ views/include/footer.php
@@ -62,6 +62,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+
 <!-- Script personalizado para activar el cambio de estilo al hacer clic -->
 <script>
   // JavaScript para activar el cambio de estilo al hacer clic
@@ -75,5 +76,11 @@
   });
 </script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+<script
+     src="https://code.jquery.com/jquery-3.7.1.js"
+     integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+     crossorigin="anonymous"></script>
+     
 </body>
 </html>
